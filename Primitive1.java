ca67898baddc2ccd71a1325fc44ac1f0449b35ec
RonijsBarenitis <74355633+RonijsBarenitis@users.noreply.github.com>
2023-10-10 16:54:12 +0200

    Add files via upload

---
+++ Primitive1.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+      double base=30;
+      double height=40;
+      double area=(base*height)/2;
+
+      System.out.println("The area of the right triangle is  " + area);
+    }
+}
